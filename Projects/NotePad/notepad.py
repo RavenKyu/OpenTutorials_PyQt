0ee86b42b9265f5b9c6d21c935d066373cf3ee34
Deokyu Lim <hong18s@gmail.com>
2017-08-12 01:34:02 +0900

    #6 [메모장 만들기] 메뉴바 추가 * 각 카테고리의 메뉴를 추가

--- Projects/NotePad/notepad.py
+++ Projects/NotePad/notepad.py
@@ -9,7 +9,10 @@ import sys
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QTranslator
+from PyQt5.QtCore import QLocale
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QEvent
 from notepad_menubar import MenuBarWidget
 
 __author__ = "Deokyu Lim <hong18s@gmail.com>"
@@ -18,19 +21,55 @@ __author__ = "Deokyu Lim <hong18s@gmail.com>"
 class Form(QMainWindow):
 	def __init__(self):
 		QMainWindow.__init__(self, flags=Qt.Window)
-		self.filename = "제목없음"
-
 		self.menu = MenuBarWidget()
+		self.translator = QTranslator()  # I18N 관련
+		self.change_locale("translate\\" + QLocale.system().name() + ".qm")  # 시스템 로케일 사용
+		self.filename = self.tr("untitled")
 		self.init_window()
+
 
 	def init_window(self):
 		"""
 		현재 위젯의 모양등을 초기화
 		"""
-		self.setWindowTitle("제목 없음 - 메모장")
+		self.set_window_title()
 		self.resize(640, 480)
 		self.setWindowIcon(QIcon("notepad.ico"))
 		self.setMenuBar(self.menu)
+
+	def set_window_title(self):
+		"""
+		윈도우 타이틀 변경
+		:return:
+		"""
+		self.setWindowTitle("{} - {}".format(self.filename, self.tr("Notepad")))
+
+	def change_locale(self, filename):
+		"""
+		번역파일을 받아서 로드
+		:param filename:
+		:return:
+		"""
+		global app
+		app.removeTranslator(self.translator)
+		self.translator.load(filename)
+		app.installTranslator(self.translator)
+
+	def retranslate_ui(self):
+		self.set_window_title()
+		self.menu.retranslate_ui()
+
+	def changeEvent(self, event):
+		"""
+		언어가 바뀜을 감시
+		:param event:
+		:return:
+		"""
+		# 들어온 이벤트 중 언어가 바뀌었는지 검사
+		if event.type() == QEvent.LanguageChange:
+			self.retranslate_ui()
+		QMainWindow.changeEvent(self, event)  # 나머지 일을 할 수 있게 넘겨줌
+
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
